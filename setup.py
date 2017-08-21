702820aab8e333c5a02ddb7d19aa07948d677f5e
TheOneHyer <theonehyer@gmail.com>
2017-08-21 16:52:15 -0600

    Added tests and argparse packages
    Added two packages with __init__ and a single
    module, ParseCommas, for later testing.

--- setup.py
+++ setup.py
@@ -28,10 +28,10 @@ __email__ = 'theonehyer@gmail.com'
 __license__ = 'GPLv3'
 __maintainer__ = 'Alex Hyer'
 __status__ = 'Planning'
-__version__ = '0.1.0a4'
+__version__ = '0.1.0a5'
 
 setup(name='arandomness',
-      version='0.1.0a4',
+      version='0.1.0a5',
       description='An arandom assortment of random modules',
       classifiers=[
           'Development Status :: 1 - Planning',
@@ -44,7 +44,7 @@ setup(name='arandomness',
       ],
       keywords='arandomness TheOneHyer',
       url='https://github.com/TheOneHyer/arandomness',
-      download_url='https://github.com/TheOneHyer/arandomness/tarball/0.1.0a4',
+      download_url='https://github.com/TheOneHyer/arandomness/tarball/0.1.0a5',
       author='Alex Hyer',
       author_email='theonehyer@gmail.com',
       license='GPLv3',
