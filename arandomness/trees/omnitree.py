aa052238c0995ae112fe750719f3833c1dfbec67
TheOneHyer <theonehyer@gmail.com>
2017-10-05 10:44:21 -0600

    Added add_children and add_parents to OmniTree
    OmntiTree now has methods for attaching additional nodes
    and avoiding duplciate node entries.

--- arandomness/trees/omnitree.py
+++ arandomness/trees/omnitree.py
@@ -25,23 +25,51 @@ __email__ = 'theonehyer@gmail.com'
 __license__ = 'GPLv3'
 __maintainer__ = 'Alex Hyer'
 __status__ = 'Planning'
-__version__ = '0.1.0a1'
+__version__ = '0.1.0a2'
 
 
 class OmniTree(object):
-    """A many-to-many tree for organizing and manipulating related data
+    """A many-to-many tree for organizing and manipulating hierarchical data
 
     Attributes:
 
+        children (list): list of OmniTree instances that are children to this
+                         node
+
         parents (list): list of OmniTree instances that are parents to this
-                        instance
+                        node
     """
 
-    def __init__(self, parents):
-        """"""
+    def __init__(self, children=None, parents=None,):
+        """Initialize node and inform connected nodes"""
 
         self.children = []
-        self.parents = parents
+        self.parents = []
 
-        for parent in parents:
-            parent.add_child(self)
+        if children is not None:
+            self.children = children
+            for child in self.children:
+                child.add_parent([self])
+
+        if parents is not None:
+            self.parents = parents
+            for parent in self.parents:
+                parent.add_children([self])
+
+    def add_children(self, children):
+        """Adds new children nodes after filtering for duplicates
+
+        Args:
+            children (list): list of OmniTree nodes to add as children
+        """
+
+        self.children += [c for c in children if c not in self.children]
+
+    def add_parents(self, parents):
+        """Adds new parent nodes after filtering for duplicates
+
+        Args:
+            parents (list): list of OmniTree nodes to add as parents
+        """
+
+        self.parents += [p for p in parents if p not in self.parents]
