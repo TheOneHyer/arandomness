72c3349db42f239396de490f8a18feb3a9bc042f
TheOneHyer <theonehyer@gmail.com>
2017-10-06 16:32:28 -0600

    Added Branch Function to OmniTree
    OmniTree now has beta-testing functions for finding and return
    tree structure. These methods are not yet final.

--- arandomness/trees/omnitree.py
+++ arandomness/trees/omnitree.py
@@ -25,7 +25,7 @@ __email__ = 'theonehyer@gmail.com'
 __license__ = 'GPLv3'
 __maintainer__ = 'Alex Hyer'
 __status__ = 'Planning'
-__version__ = '0.1.0a2'
+__version__ = '0.1.0a4'
 
 
 class OmniTree(object):
@@ -40,28 +40,23 @@ class OmniTree(object):
                         node
     """
 
-    def __init__(self, children=None, parents=None,):
-        """Initialize node and inform connected nodes
+    def __init__(self, label, children=None, parents=None,):
+        """Initialize node and inform connected nodes"""
 
-        Args:
-            children (list): list of OmniTree instances to set as children of
-                             this node at initialization
+        self._children = []
+        self._parents = []
+        self.label = label
 
-            parents (list): list of OmniTree instances to set as parents of
-                            this node at initialization
-        """
-
-        self.children = []
-        self.parents = []
-
+        # Assign children and notify them if needed
         if children is not None:
-            self.children = children
-            for child in self.children:
+            self._children = children
+            for child in self._children:
                 child.add_parent([self])
 
+        # Assign parents and notify them if needed
         if parents is not None:
-            self.parents = parents
-            for parent in self.parents:
+            self._parents = parents
+            for parent in self._parents:
                 parent.add_children([self])
 
     def add_children(self, children):
@@ -71,7 +66,7 @@ class OmniTree(object):
             children (list): list of OmniTree nodes to add as children
         """
 
-        self.children += [c for c in children if c not in self.children]
+        self._children += [c for c in children if c not in self._children]
 
     def add_parents(self, parents):
         """Adds new parent nodes after filtering for duplicates
@@ -80,4 +75,27 @@ class OmniTree(object):
             parents (list): list of OmniTree nodes to add as parents
         """
 
-        self.parents += [p for p in parents if p not in self.parents]
+        self._parents += [p for p in parents if p not in self._parents]
+
+    def find_branches(self):
+        """"""
+
+        branches = []
+        if self._children == []:  # Current node is a leaf/end node
+            return [self.label]
+        else:
+            for child in self._children:
+                branches.append([self.label] + child.find_paths())
+            return branches
+
+    def find_unique_branches(self):
+        """"""
+
+        branches = []
+        if self._children == []:  # Current node is a leaf/end node
+            return [self.label]
+        else:
+            for child in self._children:
+                for branch in child.find_paths():
+                    branches.append([self.label] + branch)
+            return branches
