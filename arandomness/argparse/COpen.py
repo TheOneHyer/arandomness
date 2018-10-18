5482bbae9854ad5c24f23fdf69713e920813ff2f
TheOneHyer <theonehyer@gmail.com>
2018-10-18 09:08:51 -0500

    Streamlined COpen Codebase
    Excess code removed from COpen.

--- arandomness/argparse/COpen.py
+++ arandomness/argparse/COpen.py
@@ -22,18 +22,14 @@ Copyright:
 
 from ..files.Open import Open as op
 from argparse import Action
-from importlib import import_module
-import io
-from os import linesep
-from warnings import warn
 
 
 __author__ = 'Alex Hyer'
 __email__ = 'theonehyer@gmail.com'
 __license__ = 'GPLv3'
 __maintainer__ = 'Alex Hyer'
-__status__ = 'Alpha'
-__version__ = '2.0.0b1'
+__status__ = 'Beta'
+__version__ = '2.0.0b2'
 
 
 class COpen(Action):
@@ -47,7 +43,7 @@ class COpen(Action):
 
         mode (str): mode to pass to (de)compression algorithm
 
-        nargs (bool): True if multiple arguments specified
+        nargs (bool): True if multiple arguments specified, must be None
 
         **kwargs (various): optional arguments to pass to argparse and algo
     """
@@ -55,47 +51,21 @@ class COpen(Action):
     def __init__(self, option_strings, dest, mode='rb', nargs=None, **kwargs):
         """Initialize class and spawn self as Base Class w/o nargs
 
-        Warns:
-            ImportError: if Open cannot import a compression library,
-                         it warns the user that it cannot open the
-                         corresponding file type
-
         Raises:
-            ValueError: if nargs is not None, Open does not accept nargs
+            ValueError: if nargs is not None, COpen does not accept nargs
         """
 
         # Only accept a single value to analyze
         if nargs is not None:
-            raise ValueError('nargs not allowed for Open')
+            raise ValueError('nargs not allowed for COpen')
 
         # Call self again but without nargs, considering the above, I don't
         # know why this is needed, but it is
         super(COpen, self).__init__(option_strings, dest, **kwargs)
 
         # Store and establish variables used in __call__
-        self.algo = io.open  # Only used as io.open in write mode
         self.kwargs = kwargs  # Pass along unused args
         self.mode = mode.lower().strip()
-        self.modules = {}  # Later populated by compression algorithms
-        self.write_mode = False if self.mode.lstrip('U')[0] == 'r' else True
-
-        # Currently supported compression algorithms
-        modules_to_import = {
-            'bz2': 'BZ2File',
-            'gzip': 'GzipFile',
-            'lzma': 'LZMAFile'
-        }
-
-        # Dynamically import compression libraries and warn about failures
-        for mod, _class in modules_to_import.items():
-            try:
-                self.modules[_class] = getattr(import_module(mod), _class)
-            except (ImportError, AttributeError) as e:
-                self.modules[_class] = open
-                warn('Cannot process {0} files due to following error:'
-                     '{1}{2}{1}You will need to install the {0} library to '
-                     'properly use these files. Currently, such files will '
-                     'open in text mode.'.format(mod, linesep, e))
 
     def __call__(self, parser, namespace, value, option_string=None, **kwargs):
         """Detects and opens compressed files
